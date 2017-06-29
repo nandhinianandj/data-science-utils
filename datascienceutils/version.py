ddad3bd20981b795947a227abbdb5b87a040f8b5
Anand Jeyahar <anand.jeyahar@kovidgroup.com>
2017-06-29 20:34:41 +0530

    minor stuff

--- datascienceutils/version.py
+++ datascienceutils/version.py
@@ -1,9 +1,9 @@
 
 # THIS FILE IS GENERATED FROM SCIPY SETUP.PY
-short_version = '1.2.12'
-version = '1.2.12'
-full_version = '1.2.12'
-git_revision = 'bb8c7c80043fa11c2ff7bb2d4daf21052a46c549'
+short_version = '1.2.13'
+version = '1.2.13'
+full_version = '1.2.13'
+git_revision = 'ca3f0255ff6f9041ada07e1596bdc4033bd84229'
 release = True
 
 if not release:
