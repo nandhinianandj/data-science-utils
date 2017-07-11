cc733b0e73a99789f531a360bbdd10e7df8f1234
anand <anand.jeyahar@gmail.com>
2017-07-12 00:28:22 +0530

    More stuff

--- datascienceutils/version.py
+++ datascienceutils/version.py
@@ -3,7 +3,7 @@
 short_version = '1.2.13'
 version = '1.2.13'
 full_version = '1.2.13'
-git_revision = 'ddad3bd20981b795947a227abbdb5b87a040f8b5'
+git_revision = '1ccc550851cc5ecb459c5fce0c3734532dfb3a5a'
 release = True
 
 if not release:
